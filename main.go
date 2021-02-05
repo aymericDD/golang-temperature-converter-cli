eaea66e7c4c9bfe29c2c2b9240636e9aa42389ad
Aymeric <Aymeric.Daurelle@cdiscount.com>
2021-02-05 21:54:40 +0100

    M2: convert_again done

--- main.go
+++ main.go
@@ -41,6 +41,12 @@ func main() {
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
+		_, err = fmt.Scanln(&shouldConvertAgain)
+
+		if err != nil {
+			printError(errReadingInput)
+		}
+
 		if shouldConvertAgain != "Y" {
 			fmt.Println("Good bye!")
 			break
